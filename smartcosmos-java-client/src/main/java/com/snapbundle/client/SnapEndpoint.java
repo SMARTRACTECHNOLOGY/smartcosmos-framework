f9b0ba4e79a0bad1067c7662ff67d77d9da92c3b
Tag Dynamics Engineering <admin@tagdynamics.net>
2013-12-25 19:37:28 -0500

    Updated endpoints

--- smartcosmos-java-client/src/main/java/com/snapbundle/client/SnapEndpoint.java
+++ smartcosmos-java-client/src/main/java/com/snapbundle/client/SnapEndpoint.java
@@ -2,11 +2,10 @@ package com.snapbundle.client;
 
 import com.snapbundle.model.base.EntityReferenceType;
 import com.snapbundle.model.context.MetadataDataType;
-import org.restlet.data.MediaType;
 
 public final class SnapEndpoint
 {
-//    public static final String PATH_REALM_CHECK = "/public/realm/%s";
+    //    public static final String PATH_REALM_CHECK = "/public/realm/%s";
     public static final String PATH_REALM_CHECK = "/registration/realm/%s";
 
 
@@ -15,7 +14,7 @@ public final class SnapEndpoint
         return String.format(PATH_REALM_CHECK, realm);
     }
 
-//    public static final String PATH_REGISTRATION = "/public/registration/register";
+    //    public static final String PATH_REGISTRATION = "/public/registration/register";
     public static final String PATH_REGISTRATION = "/registration/register";
 
     public static String registration()
@@ -30,74 +29,74 @@ public final class SnapEndpoint
         return PATH_CHANGE_PASSWORD;
     }
 
-    public static final String PATH_DEVICES = "/admin/devices";
+    public static final String PATH_DEVICES = "/devices";
 
     public static String devices()
     {
         return PATH_DEVICES;
     }
 
-    public static final String PATH_OBJECTS = "/app/objects";
+    public static final String PATH_OBJECTS = "/objects";
 
     public static String objects()
     {
         return PATH_OBJECTS;
     }
 
-    public static final String PATH_ENCODE_METADATA = "/public/metadata/mapper/encode/%s";
+    public static final String PATH_ENCODE_METADATA = "/metadata/mapper/encode/%s";
 
     public static String encodeMetadata(MetadataDataType metadataDataType)
     {
         return String.format(PATH_ENCODE_METADATA, metadataDataType);
     }
 
-    public static final String PATH_ASSIGN_METADATA = "/app/metadata/%s/%s";
+    public static final String PATH_ASSIGN_METADATA = "/metadata/%s/%s";
 
     public static String assignMetadata(EntityReferenceType entityReferenceType, String urn)
     {
         return String.format(PATH_ASSIGN_METADATA, entityReferenceType, urn);
     }
 
-    public static final String PATH_QUERY_METADATA = "/app/metadata/query/%s/%s";
+    public static final String PATH_QUERY_METADATA = "/metadata/%s/%s";
 
     public static String queryMetadata(EntityReferenceType entityReferenceType, String urn)
     {
         return String.format(PATH_QUERY_METADATA, entityReferenceType, urn);
     }
 
-    public static final String PATH_DECODE_METADATA = "/public/metadata/mapper/decode/%s";
+    public static final String PATH_DECODE_METADATA = "/metadata/mapper/decode/%s";
 
     public static String decodeMetadata(MetadataDataType metadataDataType)
     {
         return String.format(PATH_DECODE_METADATA, metadataDataType);
     }
 
-    public static final String PATH_ASSIGN_TAGS = "/app/tags/%s/%s";
+    public static final String PATH_ASSIGN_TAGS = "/tags/%s/%s";
 
     public static String tags(EntityReferenceType entityReferenceType, String urn)
     {
         return String.format(PATH_ASSIGN_TAGS, entityReferenceType, urn);
     }
 
-    public static final String PATH_OBJECT_INTERACTION = "/app/interactions";
+    public static final String PATH_OBJECT_INTERACTION = "/interactions";
 
     public static String interactions()
     {
         return PATH_OBJECT_INTERACTION;
     }
 
-    public static final String PATH_FILES = "/app/files";
+    public static final String PATH_FILES = "/files";
 
     public static String files()
     {
         return PATH_FILES;
     }
 
-    public static final String PATH_UPLOAD_FILE = "/app/files/%s/%s";
+    public static final String PATH_UPLOAD_FILE = "/files/%s";
 
-    public static String uploadFile(String fileUrn, MediaType mediaType)
+    public static String uploadFile(String fileUrn)
     {
-        return String.format(PATH_UPLOAD_FILE, fileUrn, mediaType.toString().replace('/', '-'));
+        return String.format(PATH_UPLOAD_FILE, fileUrn);
     }
 
     private SnapEndpoint()
