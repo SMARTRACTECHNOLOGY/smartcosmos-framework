fdd9dacb331f0b6a23908e8b24e35223d427b486
Tag Dynamics Engineering <admin@tagdynamics.net>
2014-01-16 14:10:03 -0500

    New self service password management endpoints

--- smartcosmos-java-client/src/main/java/com/snapbundle/client/endpoint/AccountEndpoints.java
+++ smartcosmos-java-client/src/main/java/com/snapbundle/client/endpoint/AccountEndpoints.java
@@ -8,13 +8,13 @@ public final class AccountEndpoints
 
     private static final String BASE = "/account";
 
-    private static final String CHANGE_MY_PASSWORD__POST = BASE.concat("/password?resetPassword=false&setPassword=%s");
+    private static final String CHANGE_MY_PASSWORD__POST = BASE.concat("/password/change");
 
-    private static final String RESET_MY_PASSWORD__POST = BASE.concat("/password?resetPassword=true");
+    private static final String RESET_MY_PASSWORD__POST = BASE.concat("/password/reset");
 
-    public static String changeMyPassword(String setPassword)
+    public static String changeMyPassword()
     {
-        return String.format(CHANGE_MY_PASSWORD__POST, setPassword);
+        return String.format(CHANGE_MY_PASSWORD__POST);
     }
 
     public static String resetMyPassword()
