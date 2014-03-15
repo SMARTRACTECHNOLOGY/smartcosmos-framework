0ad6c1fddd0f5924f07ed4198b154bd06999ab23
Tag Dynamics Engineering <admin@tagdynamics.net>
2014-03-15 16:39:00 -0400

    Implemented upsert command

--- smartcosmos-java-client/src/main/java/com/snapbundle/client/impl/AbstractUpsertableBaseClient.java
+++ smartcosmos-java-client/src/main/java/com/snapbundle/client/impl/AbstractUpsertableBaseClient.java
@@ -17,21 +17,15 @@
 
 package com.snapbundle.client.impl;
 
-import com.snapbundle.Field;
 import com.snapbundle.client.api.IUpsertableBaseClient;
 import com.snapbundle.client.api.ServerContext;
 import com.snapbundle.client.api.ServiceException;
+import com.snapbundle.client.impl.command.UpsertCommand;
 import com.snapbundle.pojo.base.ResponseEntity;
 import com.snapbundle.util.json.JsonUtil;
 import com.snapbundle.util.json.ViewType;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.restlet.data.Status;
-import org.restlet.ext.json.JsonRepresentation;
-import org.restlet.representation.Representation;
-import org.restlet.resource.ClientResource;
-
-import java.io.IOException;
 
 public abstract class AbstractUpsertableBaseClient<T> extends AbstractFindableBaseClient<T> implements IUpsertableBaseClient<T>
 {
@@ -55,34 +49,8 @@ public abstract class AbstractUpsertableBaseClient<T> extends AbstractFindableBa
 
     protected void upsert(JSONObject instance, String path) throws ServiceException
     {
-        // TODO: Update the HTTP status code checks to match upsert logic
-        throw new UnsupportedOperationException("HTTP status code check logic not fully implemented");
-
-
-//        ClientResource service = createClient(path);
-//
-//        try
-//        {
-//            Representation result = service.put(new JsonRepresentation(instance));
-//
-//            if (service.getStatus().equals(Status.SUCCESS_NO_CONTENT))
-//            {
-//                LOGGER.info("Successfully updated URN %s at path %s", instance.getString(Field.URN_FIELD), path);
-//            } else
-//            {
-//                JsonRepresentation jsonRepresentation = new JsonRepresentation(result);
-//                JSONObject jsonResult = jsonRepresentation.getJsonObject();
-//
-//                LOGGER.error("Unexpected HTTP status code returned: %s", service.getStatus().getCode());
-//                ResponseEntity response = JsonUtil.fromJson(jsonResult, ResponseEntity.class);
-//                throw new ServiceException(response);
-//            }
-//
-//        } catch (JSONException | IOException e)
-//        {
-//            LOGGER.error("Unexpected Exception", e);
-//            throw new ServiceException(e);
-//        }
+        UpsertCommand<ResponseEntity> command = new UpsertCommand<>(context);
+        command.call(ResponseEntity.class, path, instance);
     }
 }
 
