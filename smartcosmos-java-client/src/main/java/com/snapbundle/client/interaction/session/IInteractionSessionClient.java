d01936c704028303deb5604b9cba6f4fa5f55655
Tag Dynamics Engineering <admin@tagdynamics.net>
2014-03-22 12:48:41 -0400

    Interaction Session client impl

---
+++ smartcosmos-java-client/src/main/java/com/snapbundle/client/interaction/session/IInteractionSessionClient.java
@@ -0,0 +1,32 @@
+/*
+ * SnapBundle SDK
+ * (C) Copyright 2013-2014 Tag Dynamics, LLC (http://tagdynamics.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.snapbundle.client.interaction.session;
+
+import com.snapbundle.client.api.IUpdateableBaseClient;
+import com.snapbundle.client.api.ServiceException;
+import com.snapbundle.model.context.IObjectInteractionSession;
+import com.snapbundle.util.json.ViewType;
+
+import java.util.Collection;
+
+public interface IInteractionSessionClient extends IUpdateableBaseClient<IObjectInteractionSession>
+{
+    Collection<IObjectInteractionSession> findByNameLike(String nameLike) throws ServiceException;
+
+    Collection<IObjectInteractionSession> findByNameLike(String nameLike, ViewType viewType) throws ServiceException;
+}
