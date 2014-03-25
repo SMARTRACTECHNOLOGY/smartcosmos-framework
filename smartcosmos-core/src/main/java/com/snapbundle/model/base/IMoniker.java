ac389396767e9490824f2066d9cd1ee8bccda6ae
Tag Dynamics Engineering <admin@tagdynamics.net>
2014-03-25 19:29:59 -0400

    Updated JavaDocs to clarify how to reset a null moniker value

--- smartcosmos-core/src/main/java/com/snapbundle/model/base/IMoniker.java
+++ smartcosmos-core/src/main/java/com/snapbundle/model/base/IMoniker.java
@@ -18,10 +18,17 @@
 package com.snapbundle.model.base;
 
 /**
- * Arbitrary textual field that is never directly interpreted by the SnapBundle platform. Monikers are provided to
- * developers to store additional contextual data outside of the scope defined by the SnapBundle platform. A typical
- * usage is to store an external system alias or key, linking the SnapBundle object to an object managed and stored
- * outside of the SnapBundle platform.
+ * Arbitrary textual field that is guaranteed to <b>never</b> be directly interpreted by the platform. Monikers are
+ * provided to developers to store additional contextual data outside of the scope defined by the platform. A typical
+ * usage is to store an external system alias or key, linking the SnapBundle entity to an entity managed and stored
+ * outside of the platform.
+ * <p/>
+ * If a moniker value is assigned, it can be reset to its original (default) null value by passing in the special
+ * {@link com.snapbundle.Field#NULL_MONIKER} value. This special value is needed because the various levels of
+ * {@link com.snapbundle.util.json.ViewType} may preclude an instance from having the actual moniker value, and if that
+ * instance were submitted during an update operation it wouldn't be clear if the value was omitted in order to reduce
+ * network bandwidth consumption or if it was omitted because the developer actually wanted null assigned. The use of
+ * the special NULL_MONIKER solves this problem.
  */
 public interface IMoniker
 {
