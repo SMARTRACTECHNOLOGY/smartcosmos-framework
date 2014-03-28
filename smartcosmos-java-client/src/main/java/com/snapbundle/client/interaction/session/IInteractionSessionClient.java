8c8babf336074fe23f806834ccd1a42e8b5f6261
Tag Dynamics Engineering <engineering@tagdynamics.net>
2014-03-28 13:25:08 -0400

    JavaDoc work

--- smartcosmos-java-client/src/main/java/com/snapbundle/client/interaction/session/IInteractionSessionClient.java
+++ smartcosmos-java-client/src/main/java/com/snapbundle/client/interaction/session/IInteractionSessionClient.java
@@ -17,16 +17,36 @@
 
 package com.snapbundle.client.interaction.session;
 
-import com.snapbundle.client.impl.IUpdateableBaseClient;
 import com.snapbundle.client.api.ServiceException;
+import com.snapbundle.client.impl.IUpdateableBaseClient;
 import com.snapbundle.model.context.IObjectInteractionSession;
 import com.snapbundle.util.json.ViewType;
 
 import java.util.Collection;
 
+/**
+ * Defines or queries for {@link com.snapbundle.model.context.IObjectInteractionSession} instances.
+ */
 public interface IInteractionSessionClient extends IUpdateableBaseClient<IObjectInteractionSession>
 {
+    /**
+     * Queries for sessions with names that start with the case-sensitive pattern, returning all matches using a
+     * {@link com.snapbundle.util.json.ViewType#Standard} view.
+     *
+     * @param nameLike Case-sensitive name like pattern (do not append any special characters like % or * to the end)
+     * @return Non-null (but possibly empty) collection of sessions that have a name that matches the specified pattern
+     * @throws ServiceException
+     */
     Collection<IObjectInteractionSession> findByNameLike(String nameLike) throws ServiceException;
 
+    /**
+     * Queries for sessions with names that start with the case-sensitive pattern, returning all matches using the
+     * specified field verbosity.
+     *
+     * @param nameLike Case-sensitive name like pattern (do not append any special characters like % or * to the end)
+     * @param viewType Field verbosity
+     * @return Non-null (but possibly empty) collection of sessions that have a name that matches the specified pattern
+     * @throws ServiceException
+     */
     Collection<IObjectInteractionSession> findByNameLike(String nameLike, ViewType viewType) throws ServiceException;
 }
--- smartcosmos-java-client/src/main/java/com/snapbundle/client/metadata/IMetadataClient.java
+++ smartcosmos-java-client/src/main/java/com/snapbundle/client/metadata/IMetadataClient.java
@@ -17,9 +17,9 @@
 
 package com.snapbundle.client.metadata;
 
+import com.snapbundle.client.api.ServiceException;
 import com.snapbundle.client.impl.IDeleteableBaseClient;
 import com.snapbundle.client.impl.IUpsertableBaseClient;
-import com.snapbundle.client.api.ServiceException;
 import com.snapbundle.model.base.EntityReferenceType;
 import com.snapbundle.model.context.IMetadata;
 import com.snapbundle.model.context.MetadataDataType;
@@ -28,17 +28,86 @@ import org.json.JSONObject;
 
 import java.util.Collection;
 
+/**
+ * Defines, deletes, or queries for {@link com.snapbundle.model.context.IMetadata} instances.
+ */
 public interface IMetadataClient extends IUpsertableBaseClient<IMetadata>, IDeleteableBaseClient<IMetadata>
 {
+    /**
+     * Queries for a specific piece of metadata, by key name, under a specific entity type and reference URN, return the
+     * match using a {@link com.snapbundle.util.json.ViewType#Standard} view.
+     *
+     * @param entityReferenceType Entity reference type
+     * @param referenceUrn        Reference URN
+     * @param key                 Case-sensitive key name
+     * @return Non-null metadata value
+     * @throws ServiceException thrown if no such key under the specified entity type and reference URN exists
+     */
     IMetadata findSpecificKey(EntityReferenceType entityReferenceType, String referenceUrn, String key) throws ServiceException;
 
+    /**
+     * Queries for a specific piece of metadata, by key name, under a specific entity type and reference URN, return the
+     * match using the specified field verbosity.
+     *
+     * @param entityReferenceType Entity reference type
+     * @param referenceUrn        Reference URN
+     * @param key                 Case-sensitive key name
+     * @param viewType            Field verbosity
+     * @return Non-null metadata value
+     * @throws ServiceException thrown if no such key under the specified entity type and reference URN exists
+     */
     IMetadata findSpecificKey(EntityReferenceType entityReferenceType, String referenceUrn, String key, ViewType viewType) throws ServiceException;
 
+    /**
+     * Queries for all metadata keys under a specific entity type and reference URN, returned using
+     * a {@link com.snapbundle.util.json.ViewType#Standard} view.
+     *
+     * @param entityReferenceType Entity reference type
+     * @param referenceUrn        Reference URN
+     * @return Non-null (but possibly empty) collection of metadata definitions
+     * @throws ServiceException thrown if no such key under the specified entity type and reference URN exists
+     */
     Collection<IMetadata> findAll(EntityReferenceType entityReferenceType, String referenceUrn) throws ServiceException;
 
+    /**
+     * Queries for all metadata keys under a specific entity type and reference URN, returned using the specified field
+     * verbosity.
+     *
+     * @param entityReferenceType Entity reference type
+     * @param referenceUrn        Reference URN
+     * @param viewType            Field verbosity
+     * @return Non-null (but possibly empty) collection of metadata definitions
+     * @throws ServiceException thrown if no such key under the specified entity type and reference URN exists
+     */
     Collection<IMetadata> findAll(EntityReferenceType entityReferenceType, String referenceUrn, ViewType viewType) throws ServiceException;
 
+    /**
+     * Takes a type-safe object instance and submits it to the platform where an opaque encoding is applied and
+     * returned, suitable for inclusion as the {@link com.snapbundle.Field#RAW_VALUE_FIELD} when
+     * upserting metadata.
+     * <p/>
+     * <B>NOTE:</B> Publicly accessible method call that does not require authentication
+     *
+     * @param metadataDataType Declared data type of the instance being submitted for encoding
+     * @param instance         Instance that is of the same data type as the enum; use String for
+     *                         {@link com.snapbundle.model.context.MetadataDataType#XMLType} and
+     *                         {@link com.snapbundle.model.context.MetadataDataType#JSONType}
+     * @param <T>              Instance that matches the data type defined by the metadataDataType enum
+     * @return Encoded representation in JSON of the data provided, accessible via the {@link com.snapbundle.Field#RAW_VALUE_FIELD}
+     * @throws ServiceException
+     */
     <T> String encodeMetadata(MetadataDataType metadataDataType, T instance) throws ServiceException;
 
+    /**
+     * Takes a raw value byte array previously encoded by
+     * {@link #encodeMetadata(com.snapbundle.model.context.MetadataDataType, Object)} and decodes the value in a
+     * type-safe manner.
+     *
+     * @param metadataDataType Declared data type of the {@link com.snapbundle.Field#RAW_VALUE_FIELD} being submitted
+     *                         for decoding
+     * @param jsonObject       JSON object that contains the {@link com.snapbundle.Field#RAW_VALUE_FIELD}
+     * @return Type-safe decoded value extracted form the JSON object
+     * @throws ServiceException
+     */
     JSONObject decodeMetadata(MetadataDataType metadataDataType, JSONObject jsonObject) throws ServiceException;
 }
