cbef7efcdfadb68d368d08ac28dcc6ca6130d238
Tag Dynamics Engineering <engineering@tagdynamics.net>
2014-03-30 10:24:11 -0400

    Added missing interaction session builder

---
+++ smartcosmos-core/src/main/java/com/snapbundle/builder/InteractionSessionBuilder.java
@@ -0,0 +1,46 @@
+/*
+ * SnapBundle SDK
+ * (C) Copyright 2013-2014 Tag Dynamics, LLC (http://tagdynamics.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.snapbundle.builder;
+
+import com.google.common.base.Preconditions;
+import com.snapbundle.model.context.IObjectInteractionSession;
+import com.snapbundle.pojo.context.ObjectInteractionSession;
+
+public class InteractionSessionBuilder extends AbstractNamedObjectBuilder<IObjectInteractionSession, InteractionSessionBuilder>
+{
+    public InteractionSessionBuilder(long startTimestamp)
+    {
+        super(new ObjectInteractionSession());
+        instance.setStartTimestamp(startTimestamp);
+    }
+
+    public InteractionSessionBuilder setType(String type)
+    {
+        instance.setType(type);
+        return this;
+    }
+
+    @Override
+    protected void onValidate()
+    {
+        Preconditions.checkNotNull(instance.getType(), "type must not be null");
+        Preconditions.checkNotNull(instance.getType(), "name must not be null");
+        Preconditions.checkState(instance.getStartTimestamp() > 0, "start timestamp must be a positive value");
+    }
+}
+
