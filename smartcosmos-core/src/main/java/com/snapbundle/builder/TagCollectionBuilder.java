6d12cacc6dc4dc60c88008e8ad6905125f003ae7
Tag Dynamics Engineering <engineering@tagdynamics.net>
2014-03-30 17:14:55 -0400

    Added new tag collection builder for tag assignments

---
+++ smartcosmos-core/src/main/java/com/snapbundle/builder/TagCollectionBuilder.java
@@ -0,0 +1,40 @@
+/*
+ * SnapBundle SDK
+ * (C) Copyright 2013-2014 Tag Dynamics, LLC (http://tagdynamics.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.snapbundle.builder;
+
+import com.snapbundle.model.context.ITag;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public final class TagCollectionBuilder
+{
+    Collection<ITag> tagCollection = new ArrayList<>();
+
+    public TagCollectionBuilder addTag(String name)
+    {
+        tagCollection.add(new TagBuilder().setName(name).build());
+        return this;
+    }
+
+    public Collection<ITag> build()
+    {
+        return Collections.unmodifiableCollection(tagCollection);
+    }
+}
