caf7aa91063bca092d6841a9a054abbc8b836237
Jason Weiss <jason.weiss@smartrac-group.com>
2014-10-23 20:58:10 -0400

    Added experimental label

--- smartcosmos-core/src/main/java/net/smartcosmos/model/extension/package-info.java
+++ smartcosmos-core/src/main/java/net/smartcosmos/model/extension/package-info.java
@@ -18,6 +18,8 @@
  */
 
 /**
+ * <b>EXPERIMENTAL PACKAGE - NO PRODUCTION SUPPORT</b>
+ *
  * Defines a plugin metaphor whereby 3rd party developers can tap into an
  * account's contextual data in order to perform advanced computations that
  * are not natively supported by the SMART COSMOS Objects platform. The primary means
