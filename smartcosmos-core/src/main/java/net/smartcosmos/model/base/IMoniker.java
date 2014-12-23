7191ace2e18f4db48f0efade7ce0cc8804d23d2e
Jason Weiss <jason.weiss@smartrac-group.com>
2014-12-23 11:04:11 -0500

    automatic header management

--- smartcosmos-core/src/main/java/net/smartcosmos/model/base/IMoniker.java
+++ smartcosmos-core/src/main/java/net/smartcosmos/model/base/IMoniker.java
@@ -1,5 +1,25 @@
 package net.smartcosmos.model.base;
 
+/*
+ * *#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
+ * SMART COSMOS Platform Core SDK
+ * ===============================================================================
+ * Copyright (C) 2013 - 2014 SMARTRAC Technology Fletcher, Inc.
+ * ===============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+ */
+
 /**
  * Arbitrary textual field that is guaranteed to <b>never</b> be directly interpreted by the platform. Monikers are
  * provided to developers to store additional contextual data outside of the scope defined by the platform. A typical
