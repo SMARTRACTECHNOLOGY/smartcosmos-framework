ccff07906050041cef30873f9a5f30ea13da848e
Tim Cross <Tim.Cross@smartrac-group.com>
2015-04-30 10:30:23 +0200

    TransactionHandler implementations need default constructor, so CreateRequestHandler instances can instantiate them with ClassUtil.create(...)

--- smartcosmos-extension-api/src/main/java/net/smartcosmos/platform/base/AbstractTransactionHandler.java
+++ smartcosmos-extension-api/src/main/java/net/smartcosmos/platform/base/AbstractTransactionHandler.java
@@ -1,5 +1,25 @@
 package net.smartcosmos.platform.base;
 
+/*
+ * *#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
+ * SMART COSMOS Platform Server API
+ * ===============================================================================
+ * Copyright (C) 2013 - 2015 Smartrac Technology Fletcher, Inc.
+ * ===============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+ */
+
 import net.smartcosmos.platform.api.IContext;
 import net.smartcosmos.platform.api.transaction.ITransactionHandler;
 
@@ -15,6 +35,18 @@ public abstract class AbstractTransactionHandler implements ITransactionHandler
     {
         this.serviceId = serviceId;
         this.name = name;
+    }
+
+    /**
+     *
+     * ClassUtil.create(Class<T> clazz, String className) needs default constructor for
+     * TransacationHandler implementations.
+     *
+     * @see net.smartcosmos.platform.util.ClassUtil#create(Class<T> clazz, String className)
+     */
+    public AbstractTransactionHandler()
+    {
+
     }
 
     @Override
