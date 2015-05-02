3c1602f1a1c15189d249c16ab36c9d3df49e5f9b
Jason Weiss <jason.weiss@smartrac-group.com>
2015-05-02 04:09:07 -0400

    checkstyle fixes

--- smartcosmos-extension-api/src/main/java/net/smartcosmos/market/IExtensionProfile.java
+++ smartcosmos-extension-api/src/main/java/net/smartcosmos/market/IExtensionProfile.java
@@ -1,8 +1,24 @@
 package net.smartcosmos.market;
 
-import net.smartcosmos.model.extension.IExtension;
-import net.smartcosmos.platform.api.IService;
-import net.smartcosmos.platform.api.ext.ISmartCosmosExtension;
+/*
+ * *#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
+ * SMART COSMOS Platform Server API
+ * ===============================================================================
+ * Copyright (C) 2013 - 2015 Smartrac Technology Fletcher, Inc.
+ * ===============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+ */
 
 import java.util.List;
 
@@ -23,9 +39,10 @@ public interface IExtensionProfile extends IProfile
     ServiceType getServiceType();
 
     /**
-     * Links to either a {@link IService#getServiceId()}, {@link IExtension#getUrn()}, or
-     * {@link ISmartCosmosExtension#getExtensionId()} depending on the values of {@link #getExtensionType()} and
-     * {@link #getServiceType()}.
+     * Links to either a {@link net.smartcosmos.platform.api.IService#getServiceId()},
+     * {@link net.smartcosmos.model.extension.IExtension#getUrn()}, or
+     * {@link net.smartcosmos.platform.api.ext.ISmartCosmosExtension#getExtensionId()} depending on the values of
+     * {@link #getExtensionType()} and {@link #getServiceType()}.
      *
      * @return Unique identifier of the related extension
      */
