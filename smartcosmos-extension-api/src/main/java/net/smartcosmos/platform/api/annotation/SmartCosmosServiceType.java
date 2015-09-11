be3ff95ac00c13dd5d50f457b4d73b141f5d5d42
Will Schneider <will.schneider@smartrac-group.com>
2015-09-11 18:18:11 -0400

    MARKET-52 Create @SmartCosmosExtension Annotation

--- smartcosmos-extension-api/src/main/java/net/smartcosmos/platform/api/annotation/SmartCosmosServiceType.java
+++ smartcosmos-extension-api/src/main/java/net/smartcosmos/platform/api/annotation/SmartCosmosServiceType.java
@@ -1,15 +1,19 @@
 /*
- *  SMART COSMOS Marketplace
+ * SMART COSMOS Platform Server API
+ * ===============================================================================
+ * Copyright (C) 2013 - 2015 Smartrac Technology Fletcher, Inc.
+ * ===============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  Copyright (c) 2015, Smartrac Technology Fletcher, Inc.
- *  267 Cane Creek Rd, Fletcher, NC, 28732, USA
- *  All Rights Reserved.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *  This software is the confidential and proprietary information of
- *  Smartrac Technology Fletcher, Inc. ("Confidential Information").
- *  You shall not disclose such Confidential Information and shall
- *  use it only in accordance with the terms of the license agreement
- *  you entered into with Smartrac Technology Fletcher, Inc.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package net.smartcosmos.platform.api.annotation;
