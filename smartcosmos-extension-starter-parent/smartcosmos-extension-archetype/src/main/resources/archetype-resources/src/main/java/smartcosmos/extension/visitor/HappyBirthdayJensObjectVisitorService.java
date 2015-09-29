8ccc0bbaf57aab660dd51e3b819aa0fce4295dbc
Tim Cross <tim.cross@smartrac-group.com>
2015-09-29 16:07:27 +0200

    aligned with naming conventions, not using public figures, plus fixes to filtering for generated pom.xml. README.md

--- smartcosmos-extension-starter-parent/smartcosmos-extension-archetype/src/main/resources/archetype-resources/src/main/java/smartcosmos/extension/visitor/HappyBirthdayJensObjectVisitorService.java
+++ smartcosmos-extension-starter-parent/smartcosmos-extension-archetype/src/main/resources/archetype-resources/src/main/java/smartcosmos/extension/visitor/HappyBirthdayJensObjectVisitorService.java
@@ -9,25 +9,25 @@ import org.slf4j.LoggerFactory;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-public class HappyBirthdayKeithRichardsObjectVisitorService extends AbstractVisitor<VisitableObject>
+public class HappyBirthdayJensObjectVisitorService extends AbstractVisitor<VisitableObject>
 {
 
-    private static final Logger LOG = LoggerFactory.getLogger(HappyBirthdayKeithRichardsObjectVisitorService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(HappyBirthdayJensObjectVisitorService.class);
 
-    private Boolean onlyKeefsBirthday;
+    private Boolean onlyJensBirthday;
 
-    public HappyBirthdayKeithRichardsObjectVisitorService()
+    public HappyBirthdayJensObjectVisitorService()
     {
         // NOTE TO EXTENSION DEVELOPER:
-        // Notice that the last argument of the super constructor is present in HappyBirthdayKeithRichards,
-        // but is not in HappyBirthdayRandyWeston. HappyBirthdayRandyWeston will receive the default priority
+        // Notice that the last argument of the super constructor is present in HappyBirthdayJason.java,
+        // but is not in HappyBirthdayJens. HappyBirthdayJens will receive the default priority
         // of 50 (the range for priorities is 0 <= priority <= 100).
         //
         // Note also that the priority stored in the visitors registration table itself will not be this number,
         // but rather a much larger number that preserves your ordering and ensures that no two visitors have the
-        // same priority. If you assign two visitor with the same entity type the same priority, the order in which
-        // their visit() methods are executed will be random. If you need visitors to a particular entity type to
-        // execute in a particular order, assign them priorites.
+        // same priority. If you assign two visitors with the same entity type the same priority, the order in which
+        // their visit() methods are executed will be indeterminate. If you need visitors to a particular entity type
+        // to execute in a particular order, assign them priorites.
         //
         // NOTE TO EXTENSION DEVELOPER:
         // The UUID that constitutes the first argument of this constructor has to be unique. Don't use this string!
@@ -35,10 +35,10 @@ public class HappyBirthdayKeithRichardsObjectVisitorService extends AbstractVisi
         // java.util.UUID.getRandomUUID().toString()
         // and replace this string with that.
         //
-        super("45E7E88B-3223-4366-ABAB-E55D7A1E07F5", "Happy Birthday Object Keith Richards Visitor Service",
-              EntityReferenceType.Object, 49);
+        super("9D87E521-B5A4-4136-B42D-75A19A4C4ABD", "Happy Birthday Jens Object Visitor Service",
+              EntityReferenceType.Object);
 
-        onlyKeefsBirthday = false;
+        onlyJensBirthday = false;
 
     }
 
@@ -47,14 +47,14 @@ public class HappyBirthdayKeithRichardsObjectVisitorService extends AbstractVisi
 
         Calendar today = new GregorianCalendar();
 
-        if (onlyKeefsBirthday && (today.DAY_OF_MONTH != 18 || today.MONTH != Calendar.DECEMBER))
+        if (onlyJensBirthday && (today.DAY_OF_MONTH != 16 || today.MONTH != Calendar.APRIL))
         {
-            LOG.info("No present for Keith Richards today!");
+            LOG.info("No present for Jens today!");
             return;
         }
 
-        // either it's Keith's birthday or onlyKeefsBirthday is set to false
-        LOG.info("Happy birthday to Keith Richards from object " + object.getUrn() +
+        // either it's Jens' birthday or onlyJensBirthday is set to false
+        LOG.info("Happy birthday to Jens from object " + object.getUrn() +
                  " with name " + object.getName() + " and objectUrn " + object.getObjectUrn() +
                  " and type " + object.getType() + " and moniker " + object.getMoniker());
     }
