74e58fa9aebe799217f7c9496ad4226ac37b4289
Robert Van Voorhees <rcvanvo@gmail.com>
2015-10-01 13:43:23 -0400

    Moving this to the services implementation package.

--- smartcosmos-extension-api/src/main/java/net/smartcosmos/platform/service/impl/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This is just some very basic implementations of some of the services.  
- * 
- * Most of these are not designed for production use, but can be great for testing or development.
- *
- * @see net.smartcosmos.platform.api.service
- */
-package net.smartcosmos.platform.service.impl;
-
