f7733c70d6d7a7dcca985c6e05afba851ea6b7b2
andreassiegelrfid <andreas.siegel@smartrac-group.com>
2015-10-28 10:11:17 +0100

    Move licence header to the right place

--- smartcosmos-extension-api/src/main/java/net/smartcosmos/platform/jpa/base/DomainResourceNamedObjectEntity.java
+++ smartcosmos-extension-api/src/main/java/net/smartcosmos/platform/jpa/base/DomainResourceNamedObjectEntity.java
@@ -1,15 +1,5 @@
 package net.smartcosmos.platform.jpa.base;
 
-import com.fasterxml.jackson.annotation.JsonView;
-import net.smartcosmos.model.base.INamedObject;
-import net.smartcosmos.util.json.JsonGenerationView;
-
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.MappedSuperclass;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 /*
  * *#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
  * SMART COSMOS Platform Server API
@@ -19,9 +9,9 @@ import javax.validation.constraints.Size;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,6 +19,16 @@ import javax.validation.constraints.Size;
  * limitations under the License.
  * #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
  */
+
+import com.fasterxml.jackson.annotation.JsonView;
+import net.smartcosmos.model.base.INamedObject;
+import net.smartcosmos.util.json.JsonGenerationView;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @MappedSuperclass
 public abstract class DomainResourceNamedObjectEntity<T extends INamedObject<T>>
