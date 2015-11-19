71607ccff3ab091c1a6966c759bb72cae042feb8
andreassiegelrfid <andreas.siegel@smartrac-group.com>
2015-11-19 15:18:05 +0100

    Add IResult interface

---
+++ smartcosmos-core/src/main/java/net/smartcosmos/pojo/base/IResult.java
@@ -0,0 +1,36 @@
+package net.smartcosmos.pojo.base;
+
+/*
+ * *#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
+ * SMART COSMOS Core
+ * ===============================================================================
+ * Copyright (C) 2013 - 2015 Smartrac Technology Fletcher, Inc.
+ * ===============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+ */
+
+
+/**
+ * Created by asiegel on 19.11.2015.
+ */
+public interface IResult
+{
+    int getCode();
+    String getFormattedMessage();
+
+    /*default RESULT_ENUM_TYPE translate(int code) throws IllegalArgumentException
+    {
+        throw new IllegalArgumentException("Method not implemented");
+    }*/
+}
