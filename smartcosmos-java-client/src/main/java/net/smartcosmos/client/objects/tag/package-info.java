5882a078db845f53cc8354a83ae5d74f65013e09
Tim Cross <tim.cross@smartrac-group.com>
2016-02-23 11:11:15 +0100

    Tag functionality cleared out, moved to smartcosmos-extension-hashtag-java-client

--- smartcosmos-java-client/src/main/java/net/smartcosmos/client/objects/tag/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * {@link net.smartcosmos.objects.model.context.ITag} factory that can create a client instance with
- * {@link net.smartcosmos.client.objects.tag.TagFactory#createClient(net.smartcosmos.client.connectivity.ServerContext)}.
- */
-package net.smartcosmos.client.objects.tag;
