171f9303f12e69093e5d71687c50c725e0bb4310
Robert Van Voorhees <rcvanvo@gmail.com>
2016-05-16 11:04:52 -0400

    Removes annotation that was causing issues with bootstrapping order.

--- smartcosmos-framework/src/main/java/net/smartcosmos/security/user/SmartCosmosUserConfiguration.java
+++ smartcosmos-framework/src/main/java/net/smartcosmos/security/user/SmartCosmosUserConfiguration.java
@@ -1,20 +1,18 @@
 package net.smartcosmos.security.user;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 /**
  * @author voor
  */
 @Configuration
 @ComponentScan
-@EnableWebMvc
 public class SmartCosmosUserConfiguration extends WebMvcConfigurerAdapter {
 
     @Autowired
