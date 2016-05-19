7950a7cbadeb56312af03e76a4e1d77791c75d1a
Robert Van Voorhees <rcvanvo@gmail.com>
2016-05-19 10:43:49 -0400

    Adds in validation responses instead of empty body.

--- smartcosmos-framework/src/main/java/net/smartcosmos/security/authentication/direct/DirectExceptionHandler.java
+++ smartcosmos-framework/src/main/java/net/smartcosmos/security/authentication/direct/DirectExceptionHandler.java
@@ -1,16 +1,24 @@
 package net.smartcosmos.security.authentication.direct;
 
 import net.smartcosmos.exceptions.NoEntityFoundException;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.util.WebUtils;
+
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * Controller advice to translate the server side exceptions the media type that the
@@ -23,6 +31,9 @@ import org.springframework.web.util.WebUtils;
  */
 @ControllerAdvice
 public class DirectExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Autowired
+    MessageSource messageSource;
 
     /**
      * Customize the response for AccessDeniedException.
@@ -39,7 +50,7 @@ public class DirectExceptionHandler extends ResponseEntityExceptionHandler {
         HttpHeaders headers = new HttpHeaders();
         HttpStatus status = HttpStatus.FORBIDDEN;
 
-        pageNotFoundLogger.warn(ex.getMessage());
+        logger.warn(ex.getMessage());
 
         return handleExceptionInternal(ex, null, headers, status, request);
     }
@@ -50,9 +61,30 @@ public class DirectExceptionHandler extends ResponseEntityExceptionHandler {
         HttpHeaders headers = new HttpHeaders();
         HttpStatus status = HttpStatus.BAD_REQUEST;
 
-        pageNotFoundLogger.warn(ex.getMessage());
+        logger.warn(ex.getMessage());
 
         return handleExceptionInternal(ex, null, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+        logger.warn(ex.getMessage());
+
+        BindingResult result = ex.getBindingResult();
+        FieldError error = result.getFieldError();
+
+        return handleExceptionInternal(ex, processFieldError(error), headers, status,
+                request);
+    }
+
+    private Map<String, Object> processFieldError(FieldError error) {
+        Map<String, Object> message = new LinkedHashMap<>();
+
+        message.put("code", "-5");
+        message.put("message", messageSource.getMessage(error, Locale.ENGLISH));
+
+        return message;
     }
 
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body,
