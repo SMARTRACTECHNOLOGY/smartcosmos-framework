e379a3317b6c67f9ed8aad6f78941710fb7d0d0f
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-05-24 13:52:12 +0200

    Add Javadoc documentation

--- smartcosmos-framework/src/main/java/net/smartcosmos/exceptions/NoEntityFoundException.java
+++ smartcosmos-framework/src/main/java/net/smartcosmos/exceptions/NoEntityFoundException.java
@@ -22,16 +22,35 @@ public class NoEntityFoundException extends ServiceException {
         super(message, cause);
     }
 
-    public NoEntityFoundException(String entityType, String field, String id) {
-        super(String.format("No matching %s record with %s of %s exists", entityType, field, id));
+    /**
+     * This version of NoEntityFoundException produces an error message like
+     * <code>
+     *     {"code": -8, "message": "No matching Object record with objectUrn of urn:my-object-urn exists"}
+     * </code>
+     *
+     * @param entityType the type of the entity, e.g., Object or Metadata
+     * @param fieldName the queried field, e.g., objectUrn
+     * @param fieldValue the field value
+     */
+    public NoEntityFoundException(String entityType, String fieldName, String fieldValue) {
+        super(String.format("No matching %s record with %s of %s exists", entityType, fieldName, fieldValue));
 
-        if (!"urn".equalsIgnoreCase(field)) {
+        if (!"urn".equalsIgnoreCase(fieldName)) {
             code = ERR_RECORD_NOT_EXISTS;
         } else {
             code = ERR_UNKNOWN_ENTITY;
         }
     }
 
+    /**
+     * This version of NoEntityFoundException produces an error message like
+     * <code>
+     *     {"code": -9, "message": "Unknown Object entity with urn urn:uuid:71b81766-afb2-4be3-943f-6a24b6ae15cc"}
+     * </code>
+     *
+     * @param entityType the type of the entity, e.g., Object or Metadata
+     * @param id the entity identifier, e.g. the URN
+     */
     public NoEntityFoundException(String entityType, String id) {
         super(String.format("Unknown %s entity with urn %s", entityType, id));
         code = ERR_UNKNOWN_ENTITY;
