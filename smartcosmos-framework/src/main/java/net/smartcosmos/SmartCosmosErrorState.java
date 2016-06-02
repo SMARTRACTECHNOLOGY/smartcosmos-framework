0c8a1206ef8491a4e4d615fc301d89a57a402a7b
Will Schneider <will.schneider@smartrac-group.com>
2016-06-02 16:59:58 -0400

    replacement parameters for the strings.  UTs still required

--- smartcosmos-framework/src/main/java/net/smartcosmos/SmartCosmosErrorState.java
+++ smartcosmos-framework/src/main/java/net/smartcosmos/SmartCosmosErrorState.java
@@ -10,21 +10,21 @@ public enum SmartCosmosErrorState {
     NOT_FOUND(-2, "No such URN"),
     NOT_LICENSED(-3, "Unlicensed feature"),
     UNKNOWN_ENTITY_TYPE(-4, "Unknown Entity Reference Type"),
-    MISSING_REQUIRED_FIELD(-5, "JSON is missing a required field: <…>"),
-    DEVICE_NOT_FOUND(-6, "No device found with identification <…>"),
-    DUPLICATE_FOUND(-7, "A <…> with matching key <…> already exists"),
-    NO_MATCH(-8, "No matching record with <…> of <…> exists"),
-    ENTITY_NOT_FOUND(-9, "Unknown %s entity with urn <…>"),
-    TYPE_NOT_DEFINED(-10, "<…> enum does not define type <…>"),
+    MISSING_REQUIRED_FIELD(-5, "JSON is missing a required field: %s"),
+    DEVICE_NOT_FOUND(-6, "No device found with identification %s"),
+    DUPLICATE_FOUND(-7, "A %s with matching key %s already exists"),
+    NO_MATCH(-8, "No matching record with %s of %s exists"),
+    ENTITY_NOT_FOUND(-9, "Unknown %s entity with urn %s"),
+    TYPE_NOT_DEFINED(-10, "%s enum does not define type %s"),
     SESSION_CLOSED(-11, "session with URN %s was previously closed"),
-    USER_NOT_FOUND(-12, "No user associated with email address <…>"),
-    FILE_PENDING_UPLOAD(-13, "File URN <…> exists but is flagged as pending content upload"),
+    USER_NOT_FOUND(-12, "No user associated with email address %s"),
+    FILE_PENDING_UPLOAD(-13, "File URN %s exists but is flagged as pending content upload"),
     AUTHENTICATION_REQUIRED(-14, "Endpoint requires authentication"),
     VALIDATION_FAILURE(-15, "Input Validation Failure"),
     EMPTY_REQUEST_BODY(-16, "Request body must not be empty"),
     PARSE_FAILURE(-17, "Unable to parse input"),
     UNSUPPORTED_ENTITY_TYPE(-18, "Unsupported Entity Reference Type"),
-    INVALID_EXTENSION_OPERATION(-50, "Extensions are not permitted to perform <…>"),
+    INVALID_EXTENSION_OPERATION(-50, "Extensions are not permitted to perform %s"),
     AUTHORIZATION_FAILURE(-51, "Caller lacked the authorization to complete the requested operation"),
     INTERNAL_ERROR(-500, "Internal Server Error");
 
@@ -38,6 +38,10 @@ public enum SmartCosmosErrorState {
 
     public String getDescription() {
         return description;
+    }
+
+    public String getDescription(String...replacements) {
+        return String.format(description, replacements);
     }
 
     public int getCode() {
