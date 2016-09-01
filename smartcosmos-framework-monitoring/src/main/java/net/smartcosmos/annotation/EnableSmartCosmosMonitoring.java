a4c22d5b5aa7a213a4dd04e45b86e0166e4258a5
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-09-01 06:43:29 +0200

    Add Javadoc

--- smartcosmos-framework-monitoring/src/main/java/net/smartcosmos/annotation/EnableSmartCosmosMonitoring.java
+++ smartcosmos-framework-monitoring/src/main/java/net/smartcosmos/annotation/EnableSmartCosmosMonitoring.java
@@ -10,6 +10,11 @@ import org.springframework.context.annotation.Import;
 
 import net.smartcosmos.config.MetricsConfiguration;
 
+/**
+ * <p>Enables Smart Cosmos Monitoring configuration that automatically adds a metrics management endpoint {@code /prometheus}. The endpoint is
+ * compatible with Prometheus.</p>
+ * <p>To be used on {@link org.springframework.context.annotation.Configuration Configuration} classes.</p>
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
