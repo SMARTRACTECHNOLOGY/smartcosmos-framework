5cb4b16c64ff8ed0826e80fe6bd2addf8caa0013
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-09-26 13:59:41 +0200

    Add exception handler methods

--- smartcosmos-framework/src/main/java/net/smartcosmos/security/authentication/direct/DirectExceptionHandler.java
+++ smartcosmos-framework/src/main/java/net/smartcosmos/security/authentication/direct/DirectExceptionHandler.java
@@ -1,9 +1,17 @@
 package net.smartcosmos.security.authentication.direct;
 
-import net.smartcosmos.exceptions.NoEntityFoundException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +25,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.util.WebUtils;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
-import java.util.*;
-import java.util.stream.Collectors;
+import net.smartcosmos.exceptions.NoEntityFoundException;
 
 /**
  * Controller advice to translate the server side exceptions the media type that the
@@ -37,7 +42,9 @@ public class DirectExceptionHandler extends ResponseEntityExceptionHandler {
     @Autowired
     MessageSource messageSource;
 
+    private static final int ERR_FAILURE = -1;
     private static final int ERR_FIELD_CONSTRAINT_VIOLATION = -5;
+    private static final int ERR_VALIDATION_FAILURE = -15;
 
     /**
      * Customize the response for AccessDeniedException.
@@ -101,6 +108,48 @@ public class DirectExceptionHandler extends ResponseEntityExceptionHandler {
 
         return handleExceptionInternal(ex, processConstraintViolation(fieldNames), headers, status,
                 request);
+    }
+
+    @ExceptionHandler(ConversionFailedException.class)
+    protected ResponseEntity<?> handleConversionFailedException(ConversionFailedException ex, WebRequest request) {
+
+        if (ex.getCause() instanceof IllegalArgumentException) {
+            return handleIllegalArgumentException((IllegalArgumentException) ex.getCause(), request);
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        Map<String, Object> message = new LinkedHashMap<>();
+        message.put("code", ERR_FAILURE);
+        message.put("message", ex.toString());
+
+        logger.warn(ex.toString());
+
+        return handleExceptionInternal(ex, message, headers, status, request);
+    }
+
+    /**
+     * <p>Customize the response for {@link IllegalArgumentException} that, e.g., is thrown when invalid URNs were submitted.</p>
+     * <p>This method logs a warning and delegates to {@link #handleExceptionInternal}</p>
+     *
+     * @param ex the exception
+     * @param request the current request
+     * @return a {@code ResponseEntity} instances
+     */
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException ex, WebRequest request) {
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        Map<String, Object> message = new LinkedHashMap<>();
+        message.put("code", ERR_VALIDATION_FAILURE);
+        message.put("message", ex.getMessage());
+
+        logger.warn(ex.getMessage());
+
+        return handleExceptionInternal(ex, message, headers, status, request);
     }
 
     @Override
