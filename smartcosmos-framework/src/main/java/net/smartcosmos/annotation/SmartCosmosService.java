2054e73f3be12f732ac68452e484d652c8072dcd
Will Schneider <will.schneider@smartrac-group.com>
2016-10-04 10:22:45 -0400

    correct documentation and remove Elementtype.METHOD

--- smartcosmos-framework/src/main/java/net/smartcosmos/annotation/SmartCosmosService.java
+++ smartcosmos-framework/src/main/java/net/smartcosmos/annotation/SmartCosmosService.java
@@ -6,9 +6,13 @@ import java.lang.annotation.Target;
 import org.springframework.stereotype.Service;
 
 /**
- * Annotation use with AOP to indicate the annotated element is capavble of participating in AOP based exception handling.
+ * Annotation used with AOP to indicate the annotated element is capable of participating in AOP based exception handling.
+ * <p>
+ * When using the AOP based exception handling introduced when using this annotation all pbluc methods must throw only
+ * {@see net.smartcosmos.exceptions.SmartCosmosException}.
+ * </p>
  */
 @Service
-@Target({ ElementType.METHOD, ElementType.TYPE })
+@Target({ ElementType.TYPE })
 public @interface SmartCosmosService {
 }
